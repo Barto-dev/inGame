019fbb5739dc58a44dc09ec9b21f1064a87aa5b4
Barto <illya1004work@gmail.com>
2021-08-29 17:00:29 +0300

    favorite btn

--- src/components/Favorite/Favorite.tsx
+++ src/components/Favorite/Favorite.tsx
@@ -2,6 +2,11 @@ import React, {useState} from 'react';
 import {FavoriteInterface} from "./Favorite.interface";
 import {useActions} from "../../hooks/useActions";
 
+import styles from './Favorite.module.css';
+
+import heartIcon from './img/heart-icon.svg';
+import fillHeartIcon from './img/independ-heart-icon.svg';
+
 const Favorite = ({repoId}: FavoriteInterface): JSX.Element => {
   const [checked, setChecked] = useState<boolean>(false);
   const {addToFavoriteRepositories, deleteFavoriteRepositories} = useActions();
@@ -16,8 +21,13 @@ const Favorite = ({repoId}: FavoriteInterface): JSX.Element => {
   }
 
   return (
-    <label>
-      <input type="checkbox" checked={checked} onChange={onFavChangeHandler}/>
+    <label className={styles.favorite}>
+      <img src={checked ? fillHeartIcon : heartIcon} alt="" aria-hidden="true"/>
+      <input type="checkbox"
+             checked={checked}
+             aria-label="Add to favorite"
+             onChange={onFavChangeHandler}
+             className="visually-hidden"/>
     </label>
   );
 };
--- src/components/Repo/Repo.tsx
+++ src/components/Repo/Repo.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Favorite from "../Favorite/Favorite";
 import {RepoInterface} from "./Repo.interface";
+
 import style from './Repo.module.css';
+import repoIcon from './img/repo-icon.svg';
+import starIcon from './img/star-icon.svg';
 
 const RepositoriesItem = ({
                             avatar_url,
@@ -16,17 +19,34 @@ const RepositoriesItem = ({
   return (
     <div className={style.item}>
       <div className={style.wrapper}>
-        <img src={avatar_url}
-             alt={`${login} avatar`}
-             className={style.avatar}
-             width={50}
-             height={50} />
-        <span>{name}</span>
-        <span>{stargazers_count}</span>
+
+        <div className={style.top}>
+          <img src={avatar_url}
+               alt={`${login} avatar`}
+               className={style.avatar}
+               width={50}
+               height={50} />
+
+          <div className={style.name}>
+            <img src={repoIcon} alt="icon" aria-hidden={true} width={20} height={20} />
+            <span>{name}</span>
+          </div>
+
+          <div className={style.star}>
+            <img src={starIcon} alt="icon" aria-hidden={true} width={20} height={20} />
+            <span>{stargazers_count}</span>
+          </div>
+
+        </div>
+
         <span>{description}</span>
-        <span>{html_url}</span>
-        <span>{language}</span>
-        <Favorite repoId={id} />
+        <a href={html_url} className={style.link}>{html_url}</a>
+
+        <div className={style.bottom}>
+          <span className={style.language}>Language: {language}</span>
+          <Favorite repoId={id} />
+        </div>
+
       </div>
     </div>
   );
