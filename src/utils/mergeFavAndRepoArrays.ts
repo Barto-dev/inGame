5a1aab335b9ef4c55bb01def4a34b5af6cb712f5
Barto <illya1004work@gmail.com>
2021-08-30 14:16:45 +0300

    fix merge function

--- src/utils/mergeFavAndRepoArrays.ts
+++ src/utils/mergeFavAndRepoArrays.ts
@@ -2,12 +2,11 @@ import {Repo} from "../interfaces/github.interface";
 
 export const mergeFavAndRepoArrays = (repoList: Repo[], favList: Repo[]): Repo[] => {
   const favIdx = favList.map((favRepo) => favRepo.id);
-  const newList = repoList.map((repoItem) => {
+  return repoList.map((repoItem) => {
     if (favIdx.includes(repoItem.id)) {
       return {...repoItem, favorite: true}
-    } {
+    } else {
       return {...repoItem}
     }
-  })
-  return newList;
+  });
 }
